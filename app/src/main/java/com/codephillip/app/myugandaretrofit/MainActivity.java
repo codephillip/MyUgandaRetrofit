4a4528f84328e957f046d494409cd08ec805e433
codephillip <codephillip@gmail.com>
2016-12-22 22:59:02 +0300

    intial commit

---
+++ app/src/main/java/com/codephillip/app/myugandaretrofit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.codephillip.app.myugandaretrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
